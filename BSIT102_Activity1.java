fc2ae4ae20472e815cbf8179f60d6ab986ee13d2
jcpmascarinas <royalemascarinas@gmail.com>
2019-07-30 14:00:54 +0800

    first commit

---
+++ BSIT102_Activity1.java
@@ -0,0 +1,8 @@
+# BSIT102_Activity1 >> BSIT102_Activity1.java 
+git init
+git add .
+git config --global user.email royalemascarinas@gmail.com
+git config --global user.name jcpmascarinas
+git commit -m first commit
+cd Activity 1 and 2
+echo # BSIT102_Activity1
